f0a407cc1370485b9297cf7c00599d03d5294bb5
Michael Peng <michael@mcmoo.org>
2020-01-07 18:02:39 -0500

    Finish match scoring logic

--- src/__tests__/match.ts
+++ src/__tests__/match.ts
@@ -45,6 +45,21 @@ test('MatchEntry constructor', () => {
     expect(entry.endgame).toBe(endgame);
 });
 
+test('MatchEntry scoring 4.5.2.1 foundation', () => {    
+    const auto: AutonomousPerformance = {
+        deliveredStones: [],
+        cyclesAttempted: 1,
+        stonesOnFoundation: 0,
+        parked: ScoringResult.FAILED,
+        movedFoundation: ScoringResult.SCORED 
+    };
+    
+    const entry = new MatchEntry('Q4', 4410, AllianceColor.RED,
+        auto, kEmptyTeleOp, kEmptyEndgame);
+
+    expect(entry.getAutonomousScore()).toBe(10);
+});
+
 test('MatchEntry scoring 4.5.2.2a 1 skystone', () => {    
     const auto: AutonomousPerformance = {
         deliveredStones: [StoneType.SKYSTONE],
@@ -133,4 +148,134 @@ test('MatchEntry scoring 4.5.2.2b Sky-Stone-Stone-Sky', () => {
         auto, kEmptyTeleOp, kEmptyEndgame);
 
     expect(entry.getAutonomousScore()).toBe(10 + 2 + 2 + 2);
+});
+
+test('MatchEntry scoring 4.5.2.3 parking', () => {    
+    const auto: AutonomousPerformance = {
+        deliveredStones: [],
+        cyclesAttempted: 1,
+        stonesOnFoundation: 0,
+        parked: ScoringResult.SCORED,
+        movedFoundation: ScoringResult.FAILED 
+    };
+    
+    const entry = new MatchEntry('Q4', 4410, AllianceColor.RED,
+        auto, kEmptyTeleOp, kEmptyEndgame);
+
+    expect(entry.getAutonomousScore()).toBe(5);
+});
+
+test('MatchEntry scoring 4.5.2.4 placement', () => {    
+    const auto: AutonomousPerformance = {
+        deliveredStones: [StoneType.SKYSTONE, StoneType.STONE],
+        cyclesAttempted: 3,
+        stonesOnFoundation: 2,
+        parked: ScoringResult.FAILED,
+        movedFoundation: ScoringResult.FAILED 
+    };
+    
+    const entry = new MatchEntry('Q4', 4410, AllianceColor.RED,
+        auto, kEmptyTeleOp, kEmptyEndgame);
+
+    expect(entry.getAutonomousScore()).toBe(10 + 2 + 2*4);
+});
+
+test('MatchEntry scoring 4.5.3.1 delivery', () => {    
+    const teleop: TeleOpPerformance = {
+        stonesDelivered: 2,
+        stonesPerLevel: []
+    };
+    
+    const entry = new MatchEntry('SF2-2', 10331, AllianceColor.RED,
+        kEmptyAuto, teleop, kEmptyEndgame);
+
+    expect(entry.getTeleOpScore()).toBe(2);
+});
+
+test('MatchEntry scoring 4.5.3.2 placing', () => {    
+    const teleop: TeleOpPerformance = {
+        stonesDelivered: 0,
+        stonesPerLevel: [2]
+    };
+    
+    const entry = new MatchEntry('SF2-2', 10331, AllianceColor.RED,
+        kEmptyAuto, teleop, kEmptyEndgame);
+
+    expect(entry.getTeleOpScore()).toBe(2 + 2*1);
+});
+
+test('MatchEntry scoring 4.5.3.3 skyscraper bonus', () => {    
+    const teleop: TeleOpPerformance = {
+        stonesDelivered: 0,
+        stonesPerLevel: [1, 1, 1, 1]
+    };
+    
+    const entry = new MatchEntry('F1', 5273, AllianceColor.BLUE,
+        kEmptyAuto, teleop, kEmptyEndgame);
+
+    expect(entry.getTeleOpScore()).toBe(4 + 4*2);
+});
+
+test('MatchEntry scoring 4.5.4.1 capstone', () => {    
+    const endgame: EndgamePerformance = {
+        movedFoundation: ScoringResult.DID_NOT_TRY,
+        capstoneLevel: 3,
+        parked: ScoringResult.FAILED
+    };
+    
+    const entry = new MatchEntry('F2', 5273, AllianceColor.BLUE,
+        kEmptyAuto, kEmptyTeleOp, endgame);
+
+    expect(entry.getEndgameScore()).toBe(5 + 3);
+});
+
+test('MatchEntry scoring 4.5.4.2 foundation', () => {    
+    const endgame: EndgamePerformance = {
+        movedFoundation: ScoringResult.SCORED,
+        capstoneLevel: undefined,
+        parked: ScoringResult.FAILED
+    };
+    
+    const entry = new MatchEntry('SF2-2', 4410, AllianceColor.BLUE,
+        kEmptyAuto, kEmptyTeleOp, endgame);
+
+    expect(entry.getEndgameScore()).toBe(15);
+});
+
+test('MatchEntry scoring 4.5.4.3 parking', () => {    
+    const endgame: EndgamePerformance = {
+        movedFoundation: ScoringResult.DID_NOT_TRY,
+        capstoneLevel: undefined,
+        parked: ScoringResult.SCORED
+    };
+    
+    const entry = new MatchEntry('F1', 5273, AllianceColor.BLUE,
+        kEmptyAuto, kEmptyTeleOp, endgame);
+
+    expect(entry.getEndgameScore()).toBe(5);
+});
+
+test('MatchEntry scoring cumulative Lex7.1 F2', () => {    
+    const auto: AutonomousPerformance = {
+        deliveredStones: [StoneType.SKYSTONE, StoneType.SKYSTONE],
+        cyclesAttempted: 2,
+        stonesOnFoundation: 2,
+        parked: ScoringResult.SCORED,
+        movedFoundation: ScoringResult.SCORED
+    };
+    const teleop: TeleOpPerformance = {
+        stonesDelivered: 6,
+        stonesPerLevel: [3, 1, 1, 1, 1, 1, 1, 1]
+    };
+    const endgame: EndgamePerformance = {
+        movedFoundation: ScoringResult.SCORED,
+        capstoneLevel: undefined,
+        parked: ScoringResult.SCORED
+    };
+
+    const entry = new MatchEntry('F2', 8644, AllianceColor.RED,
+        auto, teleop, endgame);
+
+    // official final score 100, minus second parking robot in endgame
+    expect(entry.getTotalScore()).toBe(100 - 5);
 });
--- src/match.ts
+++ src/match.ts
@@ -30,20 +30,59 @@ export class MatchEntry {
 
     getAutonomousScore(): number {
         let score = 0;
+        // 4.5.2.1
+        if (this.auto.movedFoundation === ScoringResult.SCORED) {
+            score += 10;
+        }
+        // 4.5.2.2
         for (let i = 0; i < this.auto.deliveredStones.length; i++) {
             const stone = this.auto.deliveredStones[i];
-            if (stone == StoneType.SKYSTONE && i < 2) {
+            if (stone === StoneType.SKYSTONE && i < 2) {
                 score += 10;
             } else if (stone != undefined) {
                 score += 2;
             }
         }
+        // 4.5.2.3
+        if (this.auto.parked === ScoringResult.SCORED) {
+            score += 5;
+        }
+        // 4.5.2.4
+        score += this.auto.stonesOnFoundation * 4;
         return score;
     }
-    getTeleOpScore(): number { return 0; }
-    getEndgameScore(): number { return 0; }
 
-    getTotalScore(): number { return 0; }
+    getTeleOpScore(): number {
+        let score = 0;
+        // 4.5.3.1
+        score += this.teleOp.stonesDelivered;
+        // 4.5.3.2
+        score += this.teleOp.stonesPerLevel.reduce((a, b) => a + b, 0);
+        // 4.5.3.3
+        score += this.teleOp.stonesPerLevel.length * 2;
+        return score;
+    }
+
+    getEndgameScore(): number {
+        let score = 0;
+        // 4.5.4.1
+        if (this.endgame.capstoneLevel !== undefined) {
+            score += 5 + this.endgame.capstoneLevel;
+        }
+        // 4.5.4.2
+        if (this.endgame.movedFoundation === ScoringResult.SCORED) {
+            score += 15;
+        }
+        // 4.5.4.3
+        if (this.endgame.parked === ScoringResult.SCORED) {
+            score += 5;
+        }
+        return score;
+    }
+
+    getTotalScore(): number {
+        return this.getAutonomousScore() + this.getTeleOpScore() + this.getEndgameScore();
+    }
 }
 
 export interface AutonomousPerformance {
